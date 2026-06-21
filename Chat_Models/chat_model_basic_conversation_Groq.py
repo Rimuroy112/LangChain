88bde829d7edb3e2fb89326a9510c2ac2a2e690a
Rimuroy112 <titashroy112@gmail.com>
2026-06-21 23:51:16 +0600

    Chat model basic conversation groq

---
+++ Chat_Models/chat_model_basic_conversation_Groq.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain_groq import ChatGroq
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+# Create a ChatOpenAI model
+model = ChatGroq(model="llama-3.1-8b-instant",
+                   temperature = 0.7,
+                   api_key = os.getenv("GROQ_API_KEY")
+                   )
+
+# SystemMessage:
+#   Message for priming AI behavior, usually passed in as the first of a sequenc of input messages.
+# HumanMessagse:
+#   Message from a human to the AI model.
+messages = [
+    SystemMessage(content="Solve the following math problems"),
+    HumanMessage(content="What is 81 divided by 9?"),
+]
+
+# Invoke the model with messages
+result = model.invoke(messages)
+print(f"Answer from AI: {result.content}")
+
+
+# AIMessage:
+#   Message from an AI.
+messages = [
+    SystemMessage(content="Solve the following math problems"),
+    HumanMessage(content="What is 81 divided by 9?"),
+    AIMessage(content="81 divided by 9 is 9."),
+    HumanMessage(content="What is 10 times 5?"),
+]
+
+# Invoke the model with messages
+result = model.invoke(messages)
+print(f"Answer from AI: {result.content}")
+
