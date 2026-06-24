55cdb5d3526592e6f2effe488d3791ca33bb3f81
Rimuroy112 <titashroy112@gmail.com>
2026-06-25 02:54:42 +0600

    Prompt template with Groq

--- Prompt_template/prompt_template_with_chat_model.py
+++ Prompt_template/prompt_template_with_chat_model.py
@@ -10,10 +10,31 @@ model = ChatGroq(
     api_key = os.getenv("GROQ_API_KEY")
 )
 
-# Part 1: Prompt from template
-print("----Prompt from template----")
-template = "Tell me a joke about {topic}."
-prompt_template = ChatPromptTemplate.from_template(template)
-prompt = prompt_template.invoke({"topic":"dogs"})
+# # Part 1: Prompt from template
+# print("----Prompt from template----")
+# template = "Tell me a joke about {topic}."
+# prompt_template = ChatPromptTemplate.from_template(template)
+# prompt = prompt_template.invoke({"topic":"dogs"})
+# result = model.invoke(prompt)
+# print(result.content)
+
+# # Part 2: Prompt with multiple placeholders
+# template_multiple = """You are a useful assistant
+# Human: Tell me a {adjective} short story about {animal}.
+# Assistant: """
+# prompt_multiple = ChatPromptTemplate.from_template(template_multiple)
+# prompt = prompt_multiple.invoke({"adjective":"funny","animal":"dogs"})
+# result = model.invoke(prompt)
+# print(result.content)
+
+# Part 3: Prompt with system and Human messages (Using Tuples)
+messages = [
+    ("system","You are a comedian who tells joke about {topic}."),
+    ("human","Tell me {joke_count} jokes."),
+]
+prompt_template = ChatPromptTemplate.from_messages(messages)
+prompt = prompt_template.invoke({"topic":"lawyers","joke_count":3})
+
 result = model.invoke(prompt)
+print("\n----Prompt with system and Human Messages(Tuple)----\n")
 print(result.content)
