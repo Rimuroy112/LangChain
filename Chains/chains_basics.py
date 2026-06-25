57629d1754e7f4f050fc97821859bc0dc040c422
Rimuroy112 <titashroy112@gmail.com>
2026-06-26 00:10:53 +0600

    Chains Basics

---
+++ Chains/chains_basics.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+from langchain_groq import ChatGroq
+import os
+load_dotenv()
+
+model = ChatGroq(
+    model = "llama-3.1-8b-instant",
+    temperature = 0.7,
+    api_key = os.getenv("GROQ_API_KEY")
+
+)
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system","you are a comedian who tells jokes about {topic}"),
+        ("human","Tell me {joke_count} jokes.")
+    ]
+)
+chain = prompt_template | model | StrOutputParser()
+result = chain.invoke({"topic":"lawyers","joke_count":3})
